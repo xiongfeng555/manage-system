4ebf4a0b350e268e2202e95735aeecf314c4b126
xiongfeng <343138759@qq.com>
2021-06-25 20:50:04 +0800

    add treemap

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
-// vue.config.js
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
 module.exports = {
-  transpileDependencies: [
-    'vue-echarts',
-    'resize-detector'
-  ]
+  configureWebpack: {
+    plugins: [
+      new BundleAnalyzerPlugin()
+    ]
+  }
 }
