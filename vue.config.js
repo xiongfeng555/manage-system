acd2c5c129222fdb4093d6157352899d844b0b91
xiongfeng <343138759@qq.com>
2021-06-27 17:46:55 +0800

    去除analysis工具

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
-module.exports = {
-  configureWebpack: {
-    plugins: [
-      new BundleAnalyzerPlugin()
-    ]
-  }
-}
+// module.exports = {
+//   configureWebpack: {
+//     plugins: [
+//       new BundleAnalyzerPlugin()
+//     ]
+//   }
+// }
